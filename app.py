8777278d2a929c9670517f2ab28a787dd5c88785
2521003802-crypto <2521003802@sv.ufm.edu.vn>
2026-06-16 14:15:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.image("IMG_4873.PNG")
+st.image("Gemini_Generated_Image_2va9ol2va9ol2va0.png")
 #Tiêu đề app
 st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 4_SV NGUYỄN NHẬT HUY")
 
